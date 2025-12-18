b2d7b9c0e09972f9cd2987e7c4ca83c2333c5e61
shinyrainbow <kornkanok.lieng@gmail.com>
2025-12-18 09:18:48 +0700

    add

--- app/[locale]/privacy/page.tsx
+++ app/[locale]/privacy/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useTranslations } from "next-intl";
-import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/header";
+import Footer from "@/components/layout/footer";
 
 export default function PrivacyPolicyPage() {
   const t = useTranslations();
